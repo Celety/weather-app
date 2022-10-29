3e725152a2c801831bcd310fec76ce403ee4d8cd
Celety <116971721+Celety@users.noreply.github.com>
2022-10-29 19:59:21 +0200

    Created JS file

---
+++ src/index.js
@@ -0,0 +1,76 @@
+let now = new Date();
+
+// Changing date
+
+let date = now.getDate();
+
+let fullDays = [
+  "Sunday",
+  "Monday",
+  "Tuesdat",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let fullDay = fullDays[now.getDay()];
+
+let fullMonths = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let fullMonth = fullMonths[now.getMonth()];
+
+let currentDay = document.querySelector("#current-day");
+currentDay.innerHTML = `${fullDay} ${date}, ${fullMonth}`;
+
+// Changing hour
+
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let currentHour = document.querySelector("#current-hour");
+currentHour.innerHTML = `${hours}:${minutes}`;
+
+//Changing city and temperature
+
+function showTemp(response) {
+  document.querySelector("#current-city").innerHTML =
+    response.data.name.toUpperCase();
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
+function searchTemperature(newCity) {
+  let apiKey = "6bfa54f242cbb59343d4e58db578dc61";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${newCity}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemp);
+}
+
+function changeCity(event) {
+  event.preventDefault();
+  let newCity = document.querySelector("#city-new-value").value;
+  searchTemperature(newCity);
+}
+
+let city = document.querySelector("#change-city");
+city.addEventListener("submit", changeCity);
+
+searchTemperature(`warsaw`);
